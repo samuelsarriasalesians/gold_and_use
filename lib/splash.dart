a875072e147a0f7ff31c563265d2f3b09798b27e
JoelMolina <joel.molina@sarria.salesians.cat>
2025-04-03 16:54:08 +0200

    Comit de Prueba

--- lib/splash.dart
+++ lib/splash.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
-}
+} 
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
